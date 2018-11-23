75b6d99333062ea341361f3a8310f0d51ebfc115
mrodsam <marta171294@gmail.com>
2018-11-23 14:04:08 +0100

    Each team is defined by a group of 3 Pokemon and the Pokemon chosen to battle

---
+++ pokeSMART/src/pokeSMART/Team.java
@@ -0,0 +1,37 @@
+package pokeSMART;
+
+import java.util.Arrays;
+
+public class Team {
+	private Pokemon[] team;
+	private Pokemon currentPokemon;
+
+	public Team(Pokemon[] team) {
+		this.team = team;
+		this.currentPokemon = team[0];
+	}
+
+	public Pokemon[] getTeam() {
+		return team;
+	}
+
+	public void setTeam(Pokemon[] team) {
+		this.team = team;
+	}
+
+	public Pokemon getCurrentPokemon() {
+		return currentPokemon;
+	}
+
+	public void setCurrentPokemon(Pokemon currentPokemon) {
+		this.currentPokemon = currentPokemon;
+	}
+
+	@Override
+	public String toString() {
+		return "Team [team=" + Arrays.toString(team) + ", currentPokemon=" + currentPokemon.getName() + "]";
+	}
+	
+	
+
+}
