8799c9e1489302130ec40df7287b213234e61423
mrodsam <marta171294@gmail.com>
2018-12-11 18:05:58 +0100

    Constructor to read learningAutomataX.txt before start learning

--- pokeSMART/src/LearningAutomata.java
+++ pokeSMART/src/LearningAutomata.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Vector;
 
+/*Imprimir los estados y las probabilidades de las acciones en un fichero*/
 public class LearningAutomata {
 
 	final double dDecFactorLR = 0.99;
@@ -7,10 +14,39 @@ public class LearningAutomata {
 	double dLearnRate = 0.1;
 	int iNewAction;
 	int iLastAction;
+	int iNumActions = 4;
 	double dLastFunEval;
 	StateAction oPresentStateAction;
 	StateAction oLastStateAction;
 	Vector<StateAction> oVStateActions = new Vector<>(10, 5);
+
+	public LearningAutomata(int id) {
+		String cadena;
+		FileReader f = null;
+		BufferedReader b = null;
+		File file = new File("learningAutomata"+id+".txt");
+		if (file.exists()) {
+			try {
+				f = new FileReader("learningAutomata"+id+".txt");
+				b = new BufferedReader(f);
+				while ((cadena = b.readLine()) != null) {
+					double[] valActions = new double[4];
+					for (int i = 0; i < cadena.split("/")[1].split(",").length; i++) {
+						valActions[i] = Double.parseDouble(cadena.split("/")[1].split(",")[i]);
+					}
+					oVStateActions.addElement(new StateAction(cadena.split("/")[0], 4, valActions));
+				}
+			} catch (IOException e) {
+				System.out.println("No existe el fichero");
+			} finally {
+				try {
+					b.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	public int vGetNewActionAutomata(String sState, int iNActions, double dFunEval) {
 		boolean bFound;
@@ -32,7 +68,7 @@ public class LearningAutomata {
 
 		if (oLastStateAction != null) {
 			if (dFunEval - dLastFunEval > 0)
-				for (int i = 0; i < oLastStateAction.iGetNActions(); i++)
+				for (int i = 0; i < iNumActions; i++)
 					if (i == iLastAction)
 						oLastStateAction.dValAction[i] += dLearnRate * (1.0 - oLastStateAction.dValAction[i]);
 					else
@@ -56,5 +92,23 @@ public class LearningAutomata {
 			dLearnRate = dMINLearnRate;
 
 		return iNewAction;
+	}
+
+	public void writeOutputFile(int id) {
+		FileWriter file = null;
+		PrintWriter pw = null;
+
+		try {
+			file = new FileWriter("learningAutomata" + id + ".txt");
+			pw = new PrintWriter(file);
+			for (StateAction stateAction : oVStateActions) {
+				pw.println(stateAction.toString());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			pw.close();
+		}
+
 	}
 }
