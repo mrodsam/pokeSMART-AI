34b204c3f30c03b8cc1788d8b895c1b8d1bcbaee
mrodsam <marta171294@gmail.com>
2018-12-11 18:01:23 +0100

    Integer - movement constants

---
+++ pokeSMART/src/Movements.java
@@ -0,0 +1,9 @@
+
+public class Movements {
+
+	public static final int switchToBackup1 = 0;
+	public static final int switchToBackup2 = 1;
+	public static final int neutralAttack = 2;
+	public static final int typeAttack = 3;
+
+}
