8479d6cfcb9aa72c3f60b5d66913c80edaadc97e
mrodsam <marta171294@gmail.com>
2018-12-11 18:00:01 +0100

    Always 4 actions per states
    - Need to block some actions (work in progress)

--- pokeSMART/src/Player.java
+++ pokeSMART/src/Player.java
@@ -5,68 +5,48 @@ public class Player {
 	Random r = new Random(1000);
 	LearningAutomata la;
 
-	public Player(Team myTeam) {
-		this.la = new LearningAutomata();
+	public Player(Team myTeam, int id) {
+		this.la = new LearningAutomata(id);
 		this.myTeam = myTeam;
 	}
 
-	public int chooseMovement(boolean fainted, State state, int reward) {
-		if (fainted) {
-			while (true) {
-				int rndm = r.nextInt(3);
-				if (!myTeam.getTeam()[rndm].isFainted()) {
-					return rndm;
-				}
+	public int choosePokemonRandomly() {
+		while (true) {
+			int rndm = r.nextInt(3);
+			if (!myTeam.getTeam()[rndm].isFainted()) {
+				return rndm;
 			}
-		} else {
-//			int rndm = r.nextInt(3);
-//			return rndm + 1;
-			System.out.println(state.getStateName());
-			int newAction = la.vGetNewActionAutomata(state.getStateName(), state.getNumActions(), reward);
-			System.out.println("Action: " + newAction);
-			switch (state.getNumActions()) {
-			case 4: //Ninguno de mis Pokémon está desmayado
-				switch (newAction) {
-				case 0:
-					/*Cambiar a backup1*/
-					return 10;
-				case 1:
-					/*Cambiar a backup2*/
-					return 11;
-				case 2:
-					/*Ataque neutro*/
-					return 2;
-				case 3:
-					/*Ataque de tipo*/
-					return 3;
-				}
-			case 3: //Alguno de mis Pokémon está desmayado
-				switch (newAction) {
-				case 0:
-					/*Ataque neutro*/
-					return 2;
-				case 1:
-					/*Ataque de tipo*/
-					return 3;
-				case 2:
-					/*Cambiar al backup no desmayado*/
-					return 12;
-				}
-			case 2: //2 de mis Pokémon están desmayados
-				/*Ataque neutro o de tipo*/
-				return newAction + 2;
-			}
+		}
+	}
+
+	public int chooseMovement(State state, int reward) {
+		System.out.println(state.getStateName());
+		int newAction = la.vGetNewActionAutomata(state.getStateName(), 4, reward);
+		System.out.println("Action: " + newAction);
+
+		switch (newAction) {
+		case 0:
+			/* Cambiar a backup1 */
+			return 0;
+		case 1:
+			/* Cambiar a backup2 */
+			return 1;
+		case 2:
+			/* Ataque neutro */
+			return 2;
+		case 3:
+			/* Ataque de tipo */
+			return 3;
 		}
 		return 0;
 	}
 
-	public Team getMyTeam() {
+	public Team getTeam() {
 		return myTeam;
 	}
 
-	public void setMyTeam(Team myTeam) {
+	public void setTeam(Team myTeam) {
 		this.myTeam = myTeam;
 	}
-	
-	
+
 }
